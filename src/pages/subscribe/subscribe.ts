f3c9df252062d492fda20df43efca7a17231e7f7
StanEssets <stan.bell@essets.com>
2018-09-10 14:47:06 -0500

    begin subscription implementation

--- src/pages/subscribe/subscribe.ts
+++ src/pages/subscribe/subscribe.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthenticationProvider } from '../../providers/authentication/authentication';
 import { CarePlanPage } from '../careplan/careplan';
+import { LoginPage } from '../login/login';
 
 @IonicPage()
 @Component({
@@ -12,29 +13,45 @@ export class SubscribePage {
   userId: string;
   pwd: string;
   pwdVer: string;
+  myKey: string;
+  myKeyVer: string;
 
-// TODO in-app purchase for subscription
+// TODO change button label to "renew" if they're already subscribed?
+// NOTE!! don't think we can ever change the key once established, or encrypted plans wouldn't be de-cryptable
+
+// login disables subscribe button if the user is subscribed, so they can't get here
+//    but it only works after first login on app load, cause we don't know yet
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public auth: AuthenticationProvider) {
-      this.userId = navParams.get("userId");
-      this.pwd = navParams.get("pwd");
+      this.userId = this.auth.userId;
+      this.pwd = this.auth.pwd;
       console.log(this.userId, this.pwd);
+      this.initPurchase();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SubscribePage');
   }
 
-  editDone() {
+  initPurchase() {
+
+  }
+
+  subscribe() {
     // call the payment ui here
     // if successful, create a new user profile on web storage
     //  include generate a userKey or else use braintree clientKey
     this.auth.userId = this.userId;
     this.auth.pwd = this.pwd;
-    // TODO then sign them in (which might be too async?)
-    this.auth.authenticate();
-    this.navCtrl.setRoot(CarePlanPage);
+    this.auth.userKey = this.myKey;
+
+
+
+    this.navCtrl.setRoot(LoginPage);
+    // TODO OR then sign them in (which might be too async?)
+    // this.auth.authenticate();
+    // this.navCtrl.setRoot(CarePlanPage);
   }
 }
