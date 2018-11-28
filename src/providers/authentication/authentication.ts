1478f89a5c8539d0cca05f41af5a290a8031ea6f
StanEssets <stan.bell@essets.com>
2018-11-28 14:55:04 -0600

    console.logging

--- src/providers/authentication/authentication.ts
+++ src/providers/authentication/authentication.ts
@@ -52,7 +52,7 @@ export class AuthenticationProvider {
             var path = this.cpapi.apiURL + "user/" + this.userId + "?p=" + this.pwd;
             this.cpapi.getData(path).then((data) => {
                 const d = JSON.parse(data);
-                console.log('authenticate getData.then');
+                // console.log('authenticate getData.then');
                 // console.log(d);
                 if (d) {
                     this.userKey = d.key;
@@ -74,7 +74,7 @@ export class AuthenticationProvider {
                 }
             })
             .catch((err) => {
-                console.log('authenticate getData.catch');
+                // console.log('authenticate getData.catch');
                 this.userLoggedIn = false;
                 this.saveAuthState();
                 reject(this.userLoggedIn);
