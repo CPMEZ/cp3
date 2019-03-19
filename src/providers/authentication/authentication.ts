9f433c163a0ac30c36444a96651b72128d6d5980
stanbell <stan@stanbell.com>
2019-03-19 13:54:42 -0500

    1.0.8 debugging subscriptions

--- src/providers/authentication/authentication.ts
+++ src/providers/authentication/authentication.ts
@@ -335,6 +335,7 @@ export class AuthenticationProvider {
     }
 
     createSubscription(productId: string): Promise<boolean> {
+        alert('createSubscription');
         return new Promise((resolve, reject) => {
             // set up a new user on cpapi
             // console.log("createSubscription");
@@ -358,6 +359,7 @@ export class AuthenticationProvider {
             console.log('before new user post', userData);
             this.http.post(api, userData, postOptions)
                 .subscribe(data => {
+                    alert('returned from post ' + data);
                     resolve(true);
                 },
                     error => {
